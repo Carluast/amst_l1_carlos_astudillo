50531e833dcefc5141d1c2b464e4163f51dc1d4c
Calos Astudillo <carluast@espol.edu.ec>
2019-11-08 13:30:21 -0500

    Pregunta_1

---
+++ app/src/main/java/com/example/amst_l1_carlos_astudillo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.amst_l1_carlos_astudillo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.BundleCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/amst_l1_carlos_astudillo/bienestar.java
@@ -0,0 +1,14 @@
+package com.example.amst_l1_carlos_astudillo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bienestar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienestar);
+    }
+}
